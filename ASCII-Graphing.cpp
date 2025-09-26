1494eca65cfc87f66de3f15a8ca6fca8253e2077
Matthew Carr <matthewstcarr@gmail.com>
2025-09-26 19:18:22 -0400

    fix bugs with ray tracing and add support for any origin

--- ASCII-Graphing.cpp
+++ ASCII-Graphing.cpp
@@ -1,20 +1,47 @@
-#include <vector>
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include <limits>
 #include <string>
 #include <iostream>
 
-int screenWidth = 51;
-int screenHeight = 21;
-float stepSize = .1;
+struct Vector3D {
+    float x, y, z;
+};
 
-float eqConst = -9.0;
-std::vector<float> varConst = {0.0, 0.0, 0.0};
-std::vector<float> varSqrdConst = {1.0, 1.0, 1.0};
+int screenWidth = 50;
+int screenHeight = 20;
+float stepSizeAng = .2;
+std::string output = "";
+
+float eqConst = -1.0;
+Vector3D varConst = {0.0, 0.0, 0.0};
+Vector3D varSqrdConst = {-1.0, 1.0, 1.0};
+Vector3D zAxis = {0.0, 0.0, 1.0};
+
+float degToRad(float deg) {
+    return deg * (M_PI/180.0);
+}
+
+inline Vector3D crossProductUnit(const Vector3D& v1, const Vector3D& v2) {
+    float cx = v1.y * v2.z - v1.z * v2.y;
+    float cy = v1.z * v2.x - v1.x * v2.z;
+    float cz = v1.x * v2.y - v1.y * v2.x;
+    float magSq = cx*cx + cy*cy + cz*cz;
+
+    if (magSq == 0.0f) {
+        return {0.0f, 0.0f, 0.0f};
+    }
+    float invMag = 1.0f / std::sqrt(magSq);
+    return { cx * invMag, cy * invMag, cz * invMag };
+}
+float moveAng(float ox, float oy, float oz, float deg){
+    float magSide = sqrt(ox*ox + oy*oy + oz*oz);
+    return tan(degToRad(deg))*magSide;
+}
 
 float traceDist(float ox, float oy, float oz, float dx, float dy, float dz) {
-    float ax = varSqrdConst[0], ay = varSqrdConst[1], az = varSqrdConst[2];
-    float bx = varConst[0], by = varConst[1], bz = varConst[2];
+    float ax = varSqrdConst.x, ay = varSqrdConst.y, az = varSqrdConst.z;
+    float bx = varConst.x, by = varConst.y, bz = varConst.z;
     float c  = eqConst;
 
     float A = ax*dx*dx + ay*dy*dy + az*dz*dz;
@@ -33,33 +60,55 @@ float traceDist(float ox, float oy, float oz, float dx, float dy, float dz) {
     return std::numeric_limits<float>::infinity();
 }
 
-std::string output = "";
+int main() {
+    Vector3D origin = {1, 2, 3};
+    Vector3D horizV = crossProductUnit(origin, zAxis);
+    Vector3D vertV  = crossProductUnit(origin, horizV);
 
-int main(){
-    for (int i = 0; i < screenHeight; i++){
-        for (int j = 0; j < screenWidth; j++){
-            float hDisp = (i - screenHeight/2.0)*stepSize;
-            float vDisp = -(j - screenWidth/2.0)*stepSize/2.0;
-            float dist = traceDist
-            (
-                -5.0,-5.0,-5.0,
-                3-std::sqrt(2)*hDisp-std::sqrt(2)*vDisp, 3+std::sqrt(2)*hDisp-std::sqrt(2)*vDisp, 3+vDisp
-            );
-            if (dist == std::numeric_limits<float>::infinity()){
+    float verticalScale = 2.6f;
+
+    for (int i = 0; i < screenHeight; i++) {
+        for (int j = 0; j < screenWidth; j++) {
+            float hOffset = (j - screenWidth / 2.0f) * stepSizeAng;
+            float vOffset = (i - screenHeight / 2.0f) * stepSizeAng * verticalScale;
+
+            Vector3D screenPoint = {
+                0 + hOffset * horizV.x + vOffset * vertV.x,
+                0 + hOffset * horizV.y + vOffset * vertV.y,
+                0 + hOffset * horizV.z + vOffset * vertV.z
+            };
+
+            Vector3D dir = {
+                screenPoint.x - origin.x,
+                screenPoint.y - origin.y,
+                screenPoint.z - origin.z
+            };
+
+            float mag = std::sqrt(dir.x*dir.x + dir.y*dir.y + dir.z*dir.z);
+            if (mag > 0.0f) {
+                dir.x /= mag;
+                dir.y /= mag;
+                dir.z /= mag;
+            }
+
+            float dist = traceDist(origin.x, origin.y, origin.z, dir.x, dir.y, dir.z);
+
+            if (dist == std::numeric_limits<float>::infinity()) {
                 output += ".";
-            } else if (dist < 0.5){
+            } else if (dist < 0.5f) {
                 output += "@";
-            } else if (dist < 1.25){
+            } else if (dist < 1.25f) {
                 output += "%";
-            } else if (dist < 1.75){
+            } else if (dist < 2.75f) {
                 output += "#";
-            } else if (dist < 2.25){
+            } else if (dist < 4.25f) {
                 output += "+";
-            } else{
+            } else {
                 output += "=";
             }
         }
         output += "\n";
     }
+
     std::cout << output;
 }
